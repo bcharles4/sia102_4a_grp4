b73a4109d9a586675bdf5f3ff1ec0227cfaf58ea
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2024-09-25 20:13:46 +0000

    started making a django prototype

---
+++ DocuCare-Integrated_System-Django/SIA102/SIA102/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
+
+def index(request):
+    return render(request, "SIA102/index.html")
