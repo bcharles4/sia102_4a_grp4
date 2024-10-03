595ffbbe8472954c4c8eedd17ebbede24137ef22
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2024-10-03 08:16:49 +0000

    tinkering with mysql database

--- DocuCare-Integrated_System-Django/SIA102/SIA102/settings.py
+++ DocuCare-Integrated_System-Django/SIA102/SIA102/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = "SIA102.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+    
+    #"default": {
+        #"ENGINE": "django.db.backends.mysql",
+        #"NAME": "SIAproject_db",  # Replace with your database name
+        #"USER": "HyrkanDjango",        # Replace with your MySQL username
+        #"PASSWORD": "gustokongJowa_22",    # Replace with your MySQL password
+        #"HOST": "icy-apples-shout.loca.lt",            # Or use 'localhost'
+        #"PORT": "3308",                  # Default MySQL port
+    #}
 }
 
 
