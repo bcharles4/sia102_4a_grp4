86dda54d65abccfadb6191d134a4ef469cde7cc0
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2024-10-05 16:36:58 +0000

    merge

--- DocuCare-Integrated_System-Django/SIA102/SIA102/models.py
+++ DocuCare-Integrated_System-Django/SIA102/SIA102/models.py
@@ -1,5 +1,3 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    userIdnumber = models.CharField(max_length=20, unique=True, null=False)
