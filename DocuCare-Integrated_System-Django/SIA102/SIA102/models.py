79fee4b4b2c7cd040c4bfaa2b2a6951d9a8f37da
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2024-10-08 09:13:55 +0000

    started making the Nurse/User table with UserIDnumber

--- DocuCare-Integrated_System-Django/SIA102/SIA102/models.py
+++ DocuCare-Integrated_System-Django/SIA102/SIA102/models.py
@@ -1,3 +1,16 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
+from django.db import models
+from django.contrib.auth.hashers import make_password
+
+class Nurse(models.Model):
+    userIdnumber = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.userIdnumber})'
