adb51760abc441371d7083012e762e7b4587c5b9
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2024-11-17 11:41:10 +0000

    data integration on all charts Done

--- DocuCare-Integrated_System-Django/SIA102/SIA102/views.py
+++ DocuCare-Integrated_System-Django/SIA102/SIA102/views.py
@@ -12,7 +12,7 @@ from collections import Counter
 from collections import defaultdict
 from datetime import datetime
 
-ngrok = "https://5447-136-158-66-138.ngrok-free.app"
+ngrok = "https://4fce-136-158-66-138.ngrok-free.app"
 
 def login_view(request):
     if request.method == "POST":
@@ -109,7 +109,7 @@ def illness_deaths_data(request):
         if response.status_code == 200:
             patients_info = response.json()
             # Filter for deceased patients
-            deceased_patients = [p for p in patients_info if p.get('Status') == "DET"]
+            deceased_patients = [p for p in patients_info if p.get('Status') == "DEC"]
 
             # Categorize by Admitting_Diagnosis
             diagnosis_count = {}
@@ -125,6 +125,35 @@ def illness_deaths_data(request):
     # Return the data in JSON format
     return JsonResponse(diagnosis_count)
 
+def mortality_data_by_month_and_year(request):
+    try:
+        response = requests.get(f'{ngrok}/DocuCare/get_patientsInfo.php')
+        if response.status_code == 200:
+            patients_info = response.json()
+            
+            # Filter for deceased patients
+            deceased_patients = [p for p in patients_info if p.get('Status') == "DEC"]
+            
+            # Categorize deaths by month and year
+            mortality_data = {}
+            for patient in deceased_patients:
+                death_date = patient.get('Admission_Date')  # Assuming discharge date is when death is recorded
+                if death_date:
+                    date_obj = datetime.strptime(death_date, '%Y-%m-%d %H:%M:%S')
+                    year = date_obj.year
+                    month = date_obj.month - 1  # Zero-based for JavaScript compatibility
+                    
+                    if year not in mortality_data:
+                        mortality_data[year] = [0] * 12
+                    mortality_data[year][month] += 1
+        else:
+            mortality_data = {}
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        mortality_data = {}
+
+    # Return the data in JSON format
+    return JsonResponse(mortality_data)
 
 def index(request):
     return render(request, "SIA102/index.html")
